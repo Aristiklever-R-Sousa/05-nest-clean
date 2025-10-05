a16050c0e7058553bd9a077509bdecbb1442d75e
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2025-10-05 18:58:01 -0300

    chore: add config module with zod validation of the env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+  ],
   controllers: [],
   providers: [PrismaService],
 })
-export class AppModule { }
+export class AppModule {}
