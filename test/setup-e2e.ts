b44fcc49e0896fe146327427cc812dd9b59c5d2f
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2026-01-04 08:15:07 -0300

    add first test e2e

---
+++ test/setup-e2e.ts
@@ -0,0 +1,32 @@
+import { execSync } from 'child_process'
+import 'dotenv/config'
+import { PrismaClient } from 'generated/prisma'
+
+const prisma = new PrismaClient()
+
+const generateUniqueDatabaseURL = (schemaId: string) => {
+    if (!process.env.DATABASE_URL) {
+        throw new Error('DATABASE_URL is not defined')
+    }
+
+    const url = new URL(process.env.DATABASE_URL)
+
+    url.searchParams.set('schema', schemaId)
+
+    return url.toString()
+}
+
+const schemaId = `e2e_${Date.now()}`
+
+beforeAll(async () => {
+    const databaseURL = generateUniqueDatabaseURL(schemaId)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+    await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+    await prisma.$disconnect()
+})
