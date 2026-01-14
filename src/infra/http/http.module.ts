e62601e22f3d81674f73a00bb8439f2a96ba917c
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2026-01-14 19:26:17 -0300

    feat: add choose question best answer controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -23,19 +23,25 @@ import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete
 import { DeleteAnswerController } from './controllers/delete-answer.controller'
 import { FetchQuestionAnswersController } from './controllers/fetch-question-answers.controller'
 import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-cases/fetch-question-answers'
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
+import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
-    CreateQuestionController, FetchRecentQuestionsController, GetQuestionBySlugController, EditQuestionController, DeleteQuestionController,
-    AnswerQuestionController, EditAnswerController, DeleteAnswerController, FetchQuestionAnswersController
+    CreateQuestionController, FetchRecentQuestionsController, GetQuestionBySlugController,
+    EditQuestionController, DeleteQuestionController,
+    AnswerQuestionController, EditAnswerController, DeleteAnswerController,
+    FetchQuestionAnswersController, ChooseQuestionBestAnswerController,
   ],
   providers: [
     RegisterStudentUseCase, AuthenticateStudentUseCase,
-    CreateQuestionUseCase, FetchRecentQuestionsUseCase, GetQuestionBySlugUseCase, EditQuestionUseCase, DeleteQuestionUseCase,
-    AnswerQuestionUseCase, EditAnswerUseCase, DeleteAnswerUseCase, FetchQuestionAnswersUseCase,
+    CreateQuestionUseCase, FetchRecentQuestionsUseCase, GetQuestionBySlugUseCase,
+    EditQuestionUseCase, DeleteQuestionUseCase,
+    AnswerQuestionUseCase, EditAnswerUseCase, DeleteAnswerUseCase,
+    FetchQuestionAnswersUseCase, ChooseQuestionBestAnswerUseCase,
   ],
 })
 export class HttpModule { }
