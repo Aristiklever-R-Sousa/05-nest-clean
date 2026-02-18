c755fbb4b6deebb6571c2a62de955969cd553cab
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2026-02-18 15:48:02 -0300

    test: add questio repository test e2e

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,5 +1,7 @@
 import { DomainEvents } from '@/core/events/domain-events'
+import { envSchema } from '@/infra/env/env'
 import { config } from 'dotenv'
+import Redis from 'ioredis'
 
 import { execSync } from 'node:child_process'
 import { PrismaClient } from 'prisma/generated/client'
@@ -7,14 +9,22 @@ import { PrismaClient } from 'prisma/generated/client'
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
 
+const env = envSchema.parse(process.env)
+
+const redis = new Redis({
+    host: env.REDIS_HOST,
+    port: env.REDIS_PORT,
+    db: env.REDIS_DB,
+})
+
 const prisma = new PrismaClient()
 
 const generateUniqueDatabaseURL = (schemaId: string) => {
-    if (!process.env.DATABASE_URL) {
+    if (!env.DATABASE_URL) {
         throw new Error('DATABASE_URL is not defined')
     }
 
-    const url = new URL(process.env.DATABASE_URL)
+    const url = new URL(env.DATABASE_URL)
 
     url.searchParams.set('schema', schemaId)
 
@@ -26,9 +36,11 @@ const schemaId = `e2e_${Date.now()}`
 beforeAll(async () => {
     const databaseURL = generateUniqueDatabaseURL(schemaId)
 
-    process.env.DATABASE_URL = databaseURL
+    env.DATABASE_URL = databaseURL
 
     DomainEvents.shouldRun = false
+
+    await redis.flushdb()
 
     execSync('pnpm prisma migrate deploy')
 })
